2567b932ce7fb7fcc0692054e4b330f4b4ea7026
TheSerumDev <theserum.dev@gmail.com>
2022-06-30 04:39:44 +0200

    first commit

---
+++ src/main/java/me/tim/replaysystem/ReplayManager.java
@@ -0,0 +1,19 @@
+package me.tim.replaysystem;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public final class ReplayManager {
+
+    private final ReplaySystemPlugin plugin;
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
---
+++ src/main/java/me/tim/replaysystem/ReplaySystemPlugin.java
@@ -0,0 +1,24 @@
+package me.tim.replaysystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ReplaySystemPlugin extends JavaPlugin {
+
+    private ReplayManager replayManager;
+
+    @Override
+    public void onEnable() {
+        this.replayManager = new ReplayManager(this);
+
+        if (this.replayManager != null) {
+            this.replayManager.onEnable();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        if (this.replayManager != null) {
+            this.replayManager.onDisable();
+        }
+    }
+}
