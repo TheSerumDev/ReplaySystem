531659b3d679c8ad493c998b9ed3e8d711899267
Paul Hoger <paul.hoger@student.kit.edu>
2022-07-11 08:31:17 +0200

    Implement read and apply NonNull to make lombok work

--- src/main/java/me/tim/replaysystem/recordables/RecEntityMove.java
+++ src/main/java/me/tim/replaysystem/recordables/RecEntityMove.java
@@ -1,20 +1,34 @@
 package me.tim.replaysystem.recordables;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+
+import lombok.*;
 
 @RequiredArgsConstructor
+@NoArgsConstructor
 @Getter
+@ToString
 public class RecEntityMove implements EntityState {
 
-    private final int entityId;
-    private final double x;
-    private final double y;
-    private final double z;
-    private final float yaw;
-    private final float pitch;
+    @NonNull
+    private int entityId;
+
+    @NonNull
+    private double x;
+
+    @NonNull
+    private double y;
+
+    @NonNull
+    private double z;
+
+    @NonNull
+    private float yaw;
+
+    @NonNull
+    private float pitch;
 
     @Override
     public void write(DataOutputStream buffer) throws IOException {
@@ -24,6 +38,16 @@ public class RecEntityMove implements EntityState {
         buffer.writeDouble(this.z);
         buffer.writeFloat(this.yaw);
         buffer.writeFloat(this.pitch);
+    }
+
+    @Override
+    public void read(DataInputStream buffer) throws IOException {
+        this.entityId = buffer.readInt();
+        this.x = buffer.readDouble();
+        this.y = buffer.readDouble();
+        this.z = buffer.readDouble();
+        this.yaw = buffer.readFloat();
+        this.pitch = buffer.readFloat();
     }
 
     @Override
