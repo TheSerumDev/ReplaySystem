5d1d7192fbfa9f287921ed00b37ad13319133043
Paul Hoger <paul.hoger@student.kit.edu>
2022-07-11 08:30:48 +0200

    Add read to entity state interface

--- src/main/java/me/tim/replaysystem/recordables/EntityState.java
+++ src/main/java/me/tim/replaysystem/recordables/EntityState.java
@@ -1,10 +1,36 @@
 package me.tim.replaysystem.recordables;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 public interface EntityState {
     void write(DataOutputStream buffer) throws IOException;
 
+    void read(DataInputStream buffer) throws IOException;
+
     int getId();
+
+    Class<?>[] STATE_CLASSES = {
+            RecEntityMove.class,
+            RecEntitySneaking.class,
+            RecEntitySpawn.class
+    };
+
+    static EntityState createEmptyStateById(int id) {
+        for (Class<?> stateClass : STATE_CLASSES) {
+            try {
+                EntityState entityState = (EntityState) stateClass.getConstructor().newInstance();
+                if (entityState.getId() == id) {
+                    return entityState;
+                }
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                     NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        throw new IllegalArgumentException("Id could not be mapped to entity state");
+    }
 }
